232cc9cff7475c85f24dc02ad0d74edb38cdc86a
antropovandrey <antropovandrey.1994@gmail.com>
2020-04-25 15:34:09 +0500

    - добавлено выделение ячеек таблицы для последующего объединения ячеек.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 $(window).on("load", function(){
 	/*Глобальные переменные*/
-	let $selectedTable, $selectedCell;   
+	let $selectedTable, $selectedCell;
 	let buffer;
-	
+
 	/*Процедура нажатия на кнопку "Удалить"
 	Выводит окно подтверждения операции. Если пользователь
 	подтверждает удаление, то удаляет родительский элемент*/
@@ -20,22 +20,22 @@ $(window).on("load", function(){
 			}
 		});
 	}
-	
+
 	/*Процедура, добавляющая в документ кнопку "Вверх, если пользователь
 	пролистал документ вниз"*/
 	$(window).on("scroll", function(){
-		if($(this).scrollTop() > $(this).height()) 
-			$("#on-top").addClass("active-top-button"); 
-		else 
+		if($(this).scrollTop() > $(this).height())
+			$("#on-top").addClass("active-top-button");
+		else
 			$("#on-top").removeClass("active-top-button");
 	});
-	
+
 	/*Процедура пролистывания страницы вверх при нажатии
 	на кнопку "Вверх"*/
 	$("#on-top").on("click", function(){
 		$("html, body").animate({scrollTop: 0}, 500);
 	});
-	
+
 	/*Процедура, устанавливающая на выбранном элементе флаг last
 	P.S. добавление новых элементов идет после элемента с флагом last*/
 	function setContainer(){
@@ -48,7 +48,7 @@ $(window).on("load", function(){
 			}
 		});
 	}
-	
+
 	/*Процедура, поднимающая выбранный контейнер на одну позицию вверх*/
 	function upContainer(){
 		$(".up-button").each(function(){
@@ -62,7 +62,7 @@ $(window).on("load", function(){
 			}
 		});
 	}
-	
+
 	/*Процедура, опускающая выбранный контейнер на одну позицию вниз*/
 	function downContainer(){
 		$(".down-button").each(function(){
@@ -76,22 +76,21 @@ $(window).on("load", function(){
 			}
 		});
 	}
-	
+
 	/*Процедура, привязывающая к контейнеру общие сигналы*/
 	function containerEvents(){
 		setContainer();
 		upContainer();
 		downContainer();
 	}
-	
-	/*Процедура нажатия на кнопку "Добавить текст"
-	Добавляет в конце документа текстовое поле с заголовком*/
+
+	/*Процедура нажатия на кнопку "Текст"
+	Добавляет в конце документа текстовое поле*/
 	$("#create-text").on("click", function(){
 		$("#last").removeAttr("id")
 			.after("<div class = 'main-container' id = 'last'>" +
-				   "<div class = 'text-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12' contenteditable>" +
-				   "<h2>*Введите заголовок*</h2>" +
-				   "<p>*Введите текст*</p>" +
+				   "<div class = 'text-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
+				   "<p contenteditable>*Введите текст*</p>" +
 				   "</div>" +
 				   "<div class = 'delete-button-container'>" +
 				   "<button type='button' class='btn btn-link up-button'>Вверх</button>" +
@@ -103,10 +102,69 @@ $(window).on("load", function(){
 		containerEvents();
 		createDeleteButtonEventListener();
 	});
-	
+
+	/*Процедура нажатия на кнопку "Заголовок"
+	Добавляет в конце документа заголовок*/
+	$("#create-header").on("click", function(){
+		$("#last").removeAttr("id")
+			.after("<div class = 'main-container' id = 'last'>" +
+				   "<div class = 'text-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
+				   "<h2 contenteditable>*Введите заголовок*</h2>" +
+				   "</div>" +
+				   "<div class = 'delete-button-container'>" +
+				   "<button type='button' class='btn btn-link up-button'>Вверх</button>" +
+				   "<button type='button' class='btn btn-link down-button'>Вниз</button>" +
+				   "<button type='button' class='btn btn-link delete-button'>Удалить</button>" +
+				   "</div>" +
+				   "</div>");
+		//Добавляем прослушивание событий
+		containerEvents();
+		createDeleteButtonEventListener();
+	});
+
+	/*Процедура нажатия на кнопку "Подзаголовок"
+	Добавляет в конце документа подзаголовок*/
+	$("#create-subheader").on("click", function(){
+		$("#last").removeAttr("id")
+			.after("<div class = 'main-container' id = 'last'>" +
+				   "<div class = 'text-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
+				   "<h3 contenteditable>*Введите подзаголовок*</h2>" +
+				   "</div>" +
+				   "<div class = 'delete-button-container'>" +
+				   "<button type='button' class='btn btn-link up-button'>Вверх</button>" +
+				   "<button type='button' class='btn btn-link down-button'>Вниз</button>" +
+				   "<button type='button' class='btn btn-link delete-button'>Удалить</button>" +
+				   "</div>" +
+				   "</div>");
+		//Добавляем прослушивание событий
+		containerEvents();
+		createDeleteButtonEventListener();
+	});
+
+	/*Процедура нажатия на кнопку "Список"
+	Добавляет в конце документа список*/
+	$("#create-list").on("click", function(){
+		$("#last").removeAttr("id")
+			.after("<div class = 'main-container' id = 'last'>" +
+				   "<div class = 'text-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
+				   "<ul contentEditable>" +
+					 "<li>*Элемент списка*</li>" +
+					 "</ul>" +
+				   "</div>" +
+				   "<div class = 'delete-button-container'>" +
+				   "<button type='button' class='btn btn-link up-button'>Вверх</button>" +
+				   "<button type='button' class='btn btn-link down-button'>Вниз</button>" +
+				   "<button type='button' class='btn btn-link delete-button'>Удалить</button>" +
+				   "</div>" +
+				   "</div>");
+		//Добавляем прослушивание событий
+		containerEvents();
+		createDeleteButtonEventListener();
+	});
+
 	/*Процедура нажатия на кнопку "Добавить картинку"
 	Добавляет в конце документа текстовое поле с заголовком*/
-	$("#create-image").on("click", function(){						  				
+	$("#create-image").on("click", function(){
 		$("#last").removeAttr("id")
 			.after("<div class = 'main-container' id = 'last'>" +
 				   "<div class = 'img-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
@@ -124,7 +182,7 @@ $(window).on("load", function(){
 		containerEvents();
 		createDeleteButtonEventListener();
 	});
-	
+
 	/*Процедура загрузки картинки на сайт*/
 	function createImgEventListener(){
 		$(".load-img").each(function(){
@@ -144,8 +202,8 @@ $(window).on("load", function(){
 			}
 		});
 	}
-	
-	/*Процедура нажатия на кнопку "Создать таблицу"
+
+	/*Процедура нажатия на кнопку "Таблица"
 	Открывает модальное окно ввода строк и столбцов и создает по
 	этим данным соответствующую таблицу*/
 	$("#create-table").on("click", function(){
@@ -160,25 +218,17 @@ $(window).on("load", function(){
 		}
 		//Создаем таблицу
 		let table = "<div class = 'main-container' id = 'last'>" +
-					"<div class = 'table-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12' contenteditable>" +
-					"<table class = 'table table-bordered'>" +
-					"<thead class='thead-dark'>" +
-					"<tr>";	
-		for(i = 0; i < cols; i++)
-			table += "<th scope='col'></th>";					 
-		table += "</tr>" +
-				 "</thead>" +
-				 "<tbody>";				 
+					"<div class = 'table-container col-12 col-sm-12 col-md-12 col-lg-12 col-lg-12'>" +
+					"<table class = 'table table-bordered' contenteditable>";
 		for(i = 0; i < rows; i++){
 			table += "<tr>";
 			for(j = 0; j < cols; j++)
 				table += "<td></td>";
 			table += "</tr>";
-		}				
-		table += "</tbody>" +
-				 "</table>" +
+		}
+		table += "</table>" +
 				 "</div>" +
-				 "<div class = 'delete-button-container'>" + 
+				 "<div class = 'delete-button-container'>" +
 				 "<button type='button' class='btn btn-link up-button'>Вверх</button>" +
 				 "<button type='button' class='btn btn-link down-button'>Вниз</button>" +
 				 "<button type='button' class='btn btn-link delete-button'>Удалить</button>" +
@@ -186,47 +236,61 @@ $(window).on("load", function(){
 				 "</div>";
 		//Добавляем таблицу на страничку
 		$("#last").removeAttr("id")
-			.after(table);			
+			.after(table);
 		//Добавляем прослушивание событий
 		createTableEventListener();
 		containerEvents();
 		createDeleteButtonEventListener();
 	});
-	
+
 	/*Процедура, добавляющая пользовательское выпадающее
 	меню в таблицы пользователя*/
 	function createTableEventListener()
 	{
-		//Отображаем меню при нажатии ПКМ по таблице
-		$("td, th").each(function(){
+		//Добавляем выделение ячеек таблицы
+		$(".table").each(function(){
+			$(this).cellSelection();
+		});
+		$("td").each(function(){
 			if(!checkEvent($(this), "contextmenu")){
+				//Отображаем меню при нажатии ПКМ по таблице
 				$(this).on("contextmenu", function(ev){
-					$selectedTable = $(this).parent().parent().parent();
+					$selectedTable = $(this).parent().parent();
 					$selectedCell = $(this);
-				
+
 					ev.preventDefault();
 					$("#menu").offset({top: ev.clientY + $(window).scrollTop(), left: ev.clientX});
 				});
 			}
 		});
 	}
-	
+
 	/*Процедура, скрывающая выпадающее меню при нажатии ЛКМ*/
-	$(document).on('click', function(){
+	$(document).on("click", function(e){
 		$("#menu").offset({top: -1000, left: -1000});
+		//if (!$selectedTable.is(e.target)){}
 	});
-	
+
 	/*Процедура копирования данных в буфер обмена*/
 	$("#copy").on("click", function(){
-		buffer = $selectedCell.html();
+		document.execCommand("copy");
 	});
-	
+
+	/*Процедура копирования данных в буфер обмена*/
+	$("#cut").on("click", function(){
+		document.execCommand("cut");
+	});
+
 	/*Процедура вставки данных*/
 	$("#paste").on("click", function(){
-		$selectedCell
-			.append(buffer);
+		document.execCommand("paste");
 	});
-	
+
+	/*Процедура объединения ячеек*/
+	$("#merge").on("click", function(){
+		alert($selectedTable.cellSelection("getArray").length);
+	});
+
 	/*Процедура добавления строки в таблицу*/
 	$("#add-row").on("click", function(){
 		let newRow;
@@ -235,7 +299,7 @@ $(window).on("load", function(){
 		$selectedCell.parent()
 			.after("<tr>" + newRow + "</tr>");
 	});
-	
+
 	/*Процедура добавления столбца в таблицу*/
 	$("#add-col").on("click", function(){
 		let selectedCol = $selectedCell.parent().children().index($(this)) - 1;
@@ -244,12 +308,12 @@ $(window).on("load", function(){
 			$(this).find("td:eq(" + selectedCol + "), th:eq(" + selectedCol + ")").after($newCell);
 		});
 	});
-	
+
 	/*Процедура удаления строки из таблицы*/
 	$("#remove-row").on("click", function(){
 		$selectedCell.parent().remove();
 	});
-	
+
 	/*Процедура удаления столбца из таблицы*/
 	$("#remove-col").on("click", function(){
 		let selectedCol = $selectedCell.parent().children().index($(this)) - 1;
@@ -257,9 +321,9 @@ $(window).on("load", function(){
 			$(this).find("td:eq(" + selectedCol + "), th:eq(" + selectedCol + ")").remove();
 		});
 	});
-	
-	/*Процедура нажатия на кнопку "Добавить таблицу Excel"
-	Добавляет в конце документа текстовое поле с заголовком*/
+
+	/*Процедура нажатия на кнопку "html"
+	Добавляет в конце документа выбранный фрагмент html (для добавления word/excel)*/
 	$("#create-excel-table").on("click", function(){
 		$("#last").removeAttr("id")
 			.after("<div class = 'main-container' id = 'last'>" +
@@ -278,8 +342,8 @@ $(window).on("load", function(){
 		containerEvents();
 		createDeleteButtonEventListener();
 	});
-	
-	/*Процедура загрузки таблицы Excel на сайт*/
+
+	/*Процедура загрузки стороннего html на сайт*/
 	function createExcelEventListener(){
 		$(".load-xml").each(function(){
 			if(!checkEvent($(this), "change")){
@@ -307,26 +371,26 @@ function isInteger(num){
 список всех привязанных к нему событий*/
 function eventsList(element){
 	let events = element.data('events');
-	if (events !== undefined) 
+	if (events !== undefined)
 		return events;
 
 	events = $.data(element, 'events');
-	if (events !== undefined) 
+	if (events !== undefined)
 		return events;
 
 	events = $._data(element, 'events');
-	if (events !== undefined) 
+	if (events !== undefined)
 		return events;
 
 	events = $._data(element[0], 'events');
-	if (events !== undefined) 
+	if (events !== undefined)
 		return events;
 
 	return false;
 }
 
 /*Функция, принимающая на вход элемент и событие
-Провреяет, првязано ли к этому элементу данное событие*/		
+Провреяет, првязано ли к этому элементу данное событие*/
 function checkEvent(element, eventname){
 	let events,
 	ret = false;
@@ -340,5 +404,3 @@ function checkEvent(element, eventname){
 	}
 	return ret;
 }
-
-
